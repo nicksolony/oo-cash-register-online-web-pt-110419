b3593f01fda9b15207d5b0f2e6f6e0a0da416027
Nick Solonyy <nicksolony@gmail.com>
2020-02-26 02:25:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,6 @@
 class CashRegister
 require "pry"
+<<<<<<< HEAD
   attr_accessor :total, :discount, :items, :last_item
   
   def initialize (discount=nil)
@@ -7,6 +8,14 @@ require "pry"
 	@discount = discount 			# takes discount in if it's provided otherwise discount is nil
 	@items = []             		#items list is empty at the start
 	
+=======
+  attr_accessor :total, :discount, :items
+  
+  def initialize (discount=nil)
+    @total = 0      				# on initialization set total to 0
+	  @discount = discount 			# takes discount in if it's provided otherwise discount is nil
+	  @items = []             		#items list is empty at the start
+>>>>>>> b5c5252d0e0a777041538b6c8535da80d313a54b
   end
   
   def total
@@ -16,11 +25,17 @@ require "pry"
   def add_item (title,price,quantity = 1)		
   # need to add item name, price and optional 	quantity, then increase total by price * quantity.
   	  n = quantity
+<<<<<<< HEAD
 	  @last_item = 0
 	  while n > 0 do
 	  @total += price
 	  @items << title
 	  @last_item+=price
+=======
+	  while n > 0 do
+	  @total += price
+	  @items << title
+>>>>>>> b5c5252d0e0a777041538b6c8535da80d313a54b
 	  n=n-1
 	  end
   end
@@ -37,11 +52,14 @@ require "pry"
 	def items
 		@items
 	end
+<<<<<<< HEAD
 	
 	def void_last_transaction
 		
 		@total = @total - @last_item
 	
 	end
+=======
+>>>>>>> b5c5252d0e0a777041538b6c8535da80d313a54b
 
 end
